e3fe119dde26591fc88ee4d372f5bd6f478c8db2
Luca Aresu <luca.aresu90@gmail.com>
2023-02-10 19:24:57 +0100

    chore: crea struttura a moduli e aggiorna composer

--- src/CenaConDelitto/UserArea/Controller/HomeController.php
+++ src/CenaConDelitto/UserArea/Controller/HomeController.php
@@ -2,20 +2,25 @@
 
 declare(strict_types=1);
 
-namespace App\CenaConDelitto\UserArea\Controller;
+namespace CenaConDelitto\UserArea\Controller;
 
+use CenaConDelitto\Shared\Controller\ControllerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mercure\HubInterface;
 use Symfony\Component\Mercure\Update;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
 
+    #[Route('/', name: 'home')]
     public function homepage(): Response
     {
         return $this->render('base.html.twig');
     }
+
+    #[Route('/test', name: 'test')]
 
     public function test(HubInterface $hub): Response
     {
